458a60030b170891d0b0ca48136aef612252f357
sym1945 <sym1945@gmail.com>
2020-11-01 22:39:08 +0900

    Mapper propertyInfo 캐싱기능 추가

--- SqlPractice/TestMapper.cs
+++ SqlPractice/TestMapper.cs
@@ -1,18 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Reflection;
 
 namespace SqlPractice
 {
-    
+
     public class TestMapper<T>
-        where T: class
+        where T : class
     {
+        private static Dictionary<Type, Dictionary<string, PropertyInfo>> _EntityColumnCaches = new Dictionary<Type, Dictionary<string, PropertyInfo>>();
+
+        private static Dictionary<string, PropertyInfo> GetColumnInfos(Type entityType)
+        {
+            lock (_EntityColumnCaches)
+            {
+                Dictionary<string, PropertyInfo> entityColumnInfos = null;
+
+                if (_EntityColumnCaches.TryGetValue(entityType, out entityColumnInfos) == false)
+                {
+                    entityColumnInfos = new Dictionary<string, PropertyInfo>();
+
+                    foreach (var property in entityType.GetProperties())
+                    {
+                        var attr = property.GetCustomAttribute<TableColumnAttribute>();
+                        var propertyName = (attr != null) ? attr.Name : property.Name;
+
+                        entityColumnInfos.Add(attr.Name, property);
+                    }
+                }
+
+                return entityColumnInfos;
+            }
+        }
+
+
         public T MapRow(IDataReader reader)
         {
             var entityType = typeof(T);
-            var entityProperties = entityType.GetProperties();
+            var entityColumnInfos = GetColumnInfos(entityType);
 
             var count = reader.FieldCount;
             var entity = (T)Activator.CreateInstance(entityType);
@@ -20,18 +46,9 @@ namespace SqlPractice
             for (int i = 0; i < count; i++)
             {
                 var columnName = reader.GetName(i);
-                var entityProperty = entityProperties.FirstOrDefault(d =>
-                {
-                    var attr = d.GetCustomAttribute<TableColumnAttribute>();
-                    if (attr != null)
-                    {
-                        if (attr.Name == columnName)
-                            return true;
-                    }
 
-                    return d.Name == columnName;
-                });
-                if (entityProperty != null)
+                PropertyInfo entityProperty;
+                if (entityColumnInfos.TryGetValue(columnName, out entityProperty))
                 {
                     entityProperty.SetValue(entity, reader[i]);
                 }
